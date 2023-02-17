9598b8de2d5007a9b9fb36ab1a955de60c1224a8
Benjamin Gondzur <blgondzur@gmail.com>
2023-02-17 12:48:53 -0600

    added index + edit png

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -10,54 +12,50 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
+        <img 
+        src="https://metrohs.com/pantheonslogo.png"
+        width="35%">
+        </img>
+        {/* <img src="https://metrohs.com/pantheonta.png" width="100" /> */}
+        {/* <h1 className={styles.title}>
+          Pantheon<br></br>Teaching Assisstant
+        </h1> */}
+        {/* <p className={styles.description}>
+          For WWT's 2023 Sustainability STEM Student Forum
+        </p> */}
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/teacher" className={styles.card}>
+            <h3>Teacher Sign in &rarr;</h3>
+            <p></p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Link href="/student" className={styles.card}>
+            <h3>Student Sign in &rarr;</h3>
+            <p></p>
+          </Link>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+            href="https://chat.openai.com"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Learn More &rarr;</h3>
+            <p>Discover how we are using AI to keep education sustainable</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Try it &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Walk through how students and teachers will interact with chat AI
             </p>
           </a>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        Metro Hackathon Team © 2023
       </footer>
 
       <style jsx>{`
